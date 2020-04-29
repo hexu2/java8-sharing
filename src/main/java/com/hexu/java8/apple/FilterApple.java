8a277f2f53e9cacbf325e0e03217a89747dff2bd
hexu <2212925883@qq.com>
2020-04-29 08:51:20 +0800

    Java8 Sharing project

--- src/main/java/com/hexu/java8/apple/FilterApple.java
+++ src/main/java/com/hexu/java8/apple/FilterApple.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class FilterApple {
 
+    @FunctionalInterface
+    public interface  Filter{
+        boolean isTrue(Apple apple);
+    }
+
     public static List<Apple> getGreenApple(List<Apple> apples){
 
         List<Apple> appleList = new ArrayList<>();
@@ -19,12 +24,33 @@ public class FilterApple {
 
     }
 
+    public static List<Apple> findApple(List<Apple> apples, Filter filter){
+
+        List<Apple> appleList = new ArrayList<>();
+        for(Apple apple: apples){
+            if(filter.isTrue(apple)){
+                appleList.add(apple);
+            }
+        }
+
+        return appleList;
+
+    }
+
+
+
     public static void main(String[] args) {
-        List<Apple> appleList = Arrays.asList(new Apple("red", 100), new Apple("green",50));
+        List<Apple> appleList = Arrays.asList(new Apple("red", 100), new Apple("green",50),new Apple("green",50));
 
         List<Apple> greenApples = getGreenApple(appleList);
 
         System.out.println(greenApples.size());
+
+        List<Apple> greenApplesNew = findApple(appleList,(apple)->{
+            return "red".equals(apple.getColor());
+        });
+
+        System.out.println(greenApplesNew.size());
 
     }
 

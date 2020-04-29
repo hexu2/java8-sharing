271031d914edee746c11a36862e802c0606bbfa3
hexu <2212925883@qq.com>
2020-04-30 01:02:20 +0800

    Java8 Sharing project

--- src/main/java/com/hexu/java8/CreateStream.java
+++ src/main/java/com/hexu/java8/CreateStream.java
@@ -1,5 +1,9 @@
 package com.hexu.java8;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,6 +16,16 @@ public class CreateStream {
 
         System.out.println("==================");
         createStreamFromValues().forEach(System.out::println);
+
+        System.out.println("==================");
+        createStreamFromArrays().forEach(System.out::println);
+        System.out.println("==================");
+
+        System.out.println(createStreamFromFile());
+
+
+        System.out.println("==================");
+
 
     }
 
@@ -26,5 +40,26 @@ public class CreateStream {
 
     private static Stream<String> createStreamFromValues(){
         return Stream.of("Sam", "Charles", "Hexu", "Liqun", "Horace");
+    }
+
+    private static Stream<String> createStreamFromArrays(){
+
+        return Arrays.stream(new String [] {"hexu","CC"});
+    }
+
+    private static Stream<String> createStreamFromFile(){
+
+        Path path = Paths.get("/Users/hexu/Developer/WorkSpace/IDEA/java8-sharing/src/main/java/com/hexu/java8/StreamFilter.java");
+
+        try (Stream<String> streamFromFiles = Files.lines(path)) {
+            streamFromFiles.forEach(System.out::println);
+            return streamFromFiles;
+
+        }catch (IOException e) {
+
+        }
+
+        return null;
+
     }
 }
